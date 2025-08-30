023c9d6a8793abd1cb22e2664928fa9fda59aa35
silasnascimento <nascimentosilas053@gmail.com>
2025-08-30 19:13:49 +0000

    fix(deploy): Configura a base de URL para o GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Apontando para o GITHUB PAGES
+  base: '/Sistema-de-Analise-Fenologica-Automatizada-SAFA/', 
+
+  server: {
+    host: true
+  }
 });
