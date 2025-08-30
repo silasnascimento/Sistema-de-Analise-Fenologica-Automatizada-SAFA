37b5ef194319a0a0d6da7fdb4f2ca5ed0795b133
silasnascimento <nascimentosilas053@gmail.com>
2025-08-30 00:46:04 +0000

    Ajusta configuração para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Sistema-de-Analise-Fenologica-Automatizada-SAFA/', // Substitua por '/<nome-do-repositorio>/'
+  base: '/Sistema-de-Analise-Fenologica-Automatizada-SAFA/'
 })
